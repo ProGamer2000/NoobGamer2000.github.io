6d9e70c533f33856341e9e8bd66afd9aac0dd1d5
fschatbot <41202233+fschatbot@users.noreply.github.com>
2020-03-27 19:28:40 +0530

    Delete creator.js

--- Chatty/creator.js
+++
@@ -1,8 +0,0 @@
-var Scriptlist = ["menu.js", "chatty.js", "script.js"];
-var i, script;
-for (i = 0; i !== Scriptlist.length; i += 1) {
-    script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = Scriptlist[i];
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
